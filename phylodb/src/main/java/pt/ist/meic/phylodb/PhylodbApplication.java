cd02e063a76bce572baaddf5125507d57de32cb4
André Páscoa <andre@pascoa.org>
2023-06-19 04:26:51 +0100

    Fixed bug where connections would timeout.

--- phylodb/src/main/java/pt/ist/meic/phylodb/PhylodbApplication.java
+++ phylodb/src/main/java/pt/ist/meic/phylodb/PhylodbApplication.java
@@ -1,7 +1,11 @@
 package pt.ist.meic.phylodb;
 
+import org.neo4j.ogm.config.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.neo4j.Neo4jProperties;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Application entry point
@@ -9,8 +13,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PhylodbApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PhylodbApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PhylodbApplication.class, args);
+    }
 
+    @Autowired
+    Neo4jProperties properties;
+
+    @Bean
+    public org.neo4j.ogm.config.Configuration configuration() {
+        Configuration.Builder builder = new org.neo4j.ogm.config.Configuration.Builder();
+        String uri = this.properties.getUri();
+        String username = this.properties.getUsername();
+        String password = this.properties.getPassword();
+
+        builder.uri(uri);
+        builder.credentials(username, password);
+
+		return builder
+                .connectionLivenessCheckTimeout(30)
+                .build();
+    }
 }
