83b0aed590be955077410b0c3e86e33d0dd76f0f
André Páscoa <andre@pascoa.org>
2024-10-21 03:33:00 +0100

    Added sleep to launch docker before execs

--- launch-docker.sh
+++ launch-docker.sh
@@ -7,6 +7,10 @@ cd "$PROJ_ROOT" || exit  # cd current directory
 cd "instance1" || exit
 USER=$(id -u):$(id -g) DB_PATH=$PROJ_ROOT/instance1/db APP_PATH=$PROJ_ROOT/instance1/app docker compose up -d
 
+echo "Doing Heavy lifting, please stand by ('-')7"
+
+sleep 120
+
 docker exec -ti phylodb-db-1 bash -c "cypher-shell -u neo4j -p password < /scripts/init/init_schema.cypher"
 docker exec -ti phylodb-db-1 bash -c "cypher-shell -u neo4j -p password < /scripts/init/init_data.cypher"
 
