4018817abfbf909f8b9fe20788c513d027766be8
André Páscoa <andre@pascoa.org>
2024-10-21 03:24:54 +0100

    Added commands for automatically adding schema and data to db.

--- launch-docker.sh
+++ launch-docker.sh
@@ -2,29 +2,12 @@
 
 # Move into current script's directory.
 PROJ_ROOT="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
-pushd "$PROJ_ROOT"  # cd current directory
+cd "$PROJ_ROOT" || exit  # cd current directory
 
-# newgrp docker
-
-cd "instance1"
-#USER=$(id -u):$(id -g) DB_PATH=$HOME/instance1/db APP_PATH=$HOME/instance1/app docker-compose up -d
+cd "instance1" || exit
 USER=$(id -u):$(id -g) DB_PATH=$PROJ_ROOT/instance1/db APP_PATH=$PROJ_ROOT/instance1/app docker compose up -d
 
-# Create a session within the running Docker container.
+docker exec -ti phylodb-db-1 bash -c "cypher-shell -u neo4j -p password < /scripts/init/init_schema.cypher"
+docker exec -ti phylodb-db-1 bash -c "cypher-shell -u neo4j -p password < /scripts/init/init_data.cypher"
 
-#docker exec -ti phylodb-sourcegit_db_1 sh -c "cypher-shell -u neo4j -p password"
-
-SCHEMA_QUERY_FILE="$PROJ_ROOT/scripts/init/init_schema.cypher"
-#docker exec -ti phylodb-sourcegit_db_1 sh -c "cypher-shell -u neo4j -p password < $(cat $SCHEMA_QUERY_FILE)"
-
-# Create admin user in Neo4j.
-#TODO: run the .cypher file from this script using docker exec.
-
-# Insert some data in Neo4j.
-#TODO: run the .cypher file from this script using docker exec.
-
-
-# Change back to original group ID.
-newgrp
-
-popd
+cd ..
